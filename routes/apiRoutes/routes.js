e8f17d45d0780b5ac29bbce3863efb80c8895755
Lauren Groh <legroh@uwm.edu>
2021-11-07 18:01:06 -0600

    update router.get and router.post with fsReadFileSync and fsWriteFileSync

--- routes/apiRoutes/routes.js
+++ routes/apiRoutes/routes.js
@@ -1,14 +1,24 @@
 // Dependencies
 const router = require('express').Router();
 const fs = require('fs');
-const { db } = require('../../data/db.json');
-const xyz = require('../../data/db.json')
-const util = require('util');
-console.log(db);
-const readFile = util.promisify(fs.readFile);
+const path = require('path');
+// const util = require('util');
+// const { db } = require('../../data/db.json');
+const db = require('../../data/db.json');
+// const readFile = util.promisify(fs.readFile);
+const { v4: uuidv4 } = require('uuid');
 
-// Retrieve existing notes from server and display on client
+// Retrieve existing notes from server 
+// const savedNotes = fs.readFileSync(db, 'utf-8');
+// if(savedNotes) {
+//     let previousNotes = JSON.parse(savedNotes);
+//     no
+// }
+
+// Display existing notes on client
 router.get('/notes', (req, res) => {
+    let notes = JSON.parse(fs.readFileSync('./data/db.json', 'utf-8'));
+    console.log(notes);
     // function readNote(){
     //     return readFile(xyz, "utf8")
     // }
@@ -16,14 +26,18 @@ router.get('/notes', (req, res) => {
     //     let note = [];
     //     note.concat(JSON.parse(notes)
     //     )
-        res.json(xyz);
-    // })
+        res.json(notes);
 });
 
 
 // Listens for new notes on client and then sends those notes to the server
 router.post('/notes', (req, res) => {
     // req.body.id = notes
+    const newNote = req.body;
+    newNote.id = uuidv4();
+    let notes = JSON.parse(fs.readFileSync('./data/db.json', 'utf-8'));
+    notes.push(newNote);
+    fs.writeFileSync('./data/db.json', JSON.stringify(notes));
     // const newNote = {
     //     title:
     //     id:
@@ -31,9 +45,5 @@ router.post('/notes', (req, res) => {
     // }
     console.log(req.body);
 }); 
-
-
-
-
 
 module.exports = router;
